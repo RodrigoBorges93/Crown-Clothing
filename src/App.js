5e0aa798f98774c82550ec229bfce2e5ee7ed423
Rodrigo Borges <rodrigoborges93@gmail.com>
2019-12-27 16:42:29 +0000

    Scratch homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import HomePage from './homepage.component'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <HomePage />
       </div>
     );
   }
