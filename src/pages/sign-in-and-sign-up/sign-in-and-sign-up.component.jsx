7928143cf3ded54634d294283790b7be1f71ebb1
Rodrigo dos Santos Borges <rodrigoborges93@gmail.com>
2020-01-04 11:57:47 -0300

    added redux, and implemented userReducer and userActions to header and app components

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 import './sign-in-and-sign-up.styles.scss';
+import { connect } from 'react-redux';
 
-const SignInAndSignUpPage = () => (
+
+
+const SignInAndSignUpPage = ({ currentUser }) => (
+
   <div className='sign-in-and-sign-up'>
-    <SignIn />
-    <SignUp />
+  <SignIn />
+  <SignUp />
+  <h1>{currentUser}</h1>
   </div>
+
 )
 
-export default SignInAndSignUpPage
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(SignInAndSignUpPage);
