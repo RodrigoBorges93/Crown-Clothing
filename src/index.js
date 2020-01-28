6ce803238b51c45b725626c3d6912d49ee34a93d
Rodrigo Borges <rodrigoborges93@gmail.com>
2020-01-28 14:42:43 +0000

    Fixing logger issues

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
-import { persistStore } from 'redux-persist';
 
 ReactDOM.render(
   <Provider store={store}>
